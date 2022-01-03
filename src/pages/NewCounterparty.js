fa73b8aca46772a99cec0e8d08cee53e8369130e
Ng Ka Fong <richardng1126@gmail.com>
2022-01-04 01:23:51 +0800

    add new counterparty page: searching

--- src/pages/NewCounterparty.js
+++ src/pages/NewCounterparty.js
@@ -3,11 +3,17 @@ import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import SearchIcon from '@material-ui/icons/Search';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import classNames from "classnames";
 import { colors } from "@material-ui/core";
+import debounce from 'lodash/debounce';
+import { useState } from 'react';
+import axios from "axios";
+import { serverURL } from '../constants'
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
   page: {
@@ -33,6 +39,17 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
     height: 300
+  },
+  autocomplete: {
+    margin: 'auto',
+    marginBottom: 16,
+    '& .MuiTextField-root .MuiInput-root':{
+      paddingRight: 0
+    }
+  },
+  description:{
+    fontSize: 'small',
+    color: colors.grey[600]
   }
 }));
 
@@ -40,20 +57,59 @@ const NewCounterparty = (props) => {
 
   const classes = useStyles();
   const { history } = props;
+  const [ options, setOptions ] = useState([]);
+
+  function search(query){
+    axios.get(serverURL+`counterparty/search?query=${query}`)
+      .then((response)=>{
+        setOptions(response.data.result)
+      })
+      .catch((error)=>{
+        console.log("TODO error catching")
+      })
+
+  }
+
+  function handleInputChange(evt, value){
+    if (value) search(value);
+  }
+
+  function renderInput(params){
+    return (
+    <TextField {...params} 
+      label="Search Counterparty"
+      InputProps={{
+        ...params.InputProps,
+        endAdornment: (
+          <InputAdornment position="end">
+            <SearchIcon />
+          </InputAdornment>
+        )
+      }}
+    />)
+  }
+
+  function renderOption(option){
+    return(
+    <Box>
+      {option.symbol} - <span className={classes.description}>{option.description}</span>
+    </Box>
+    )
+  }
 
   return (
     <div className={classes.page}>
       <Typography variant="h5">New Counterparty</Typography>
-      <TextField
-        label="Counterparty"
-        variant="outlined"
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <SearchIcon />
-            </InputAdornment>
-          )
-        }}
+      <Autocomplete
+        style={{ width: 400 }}
+        options={options}
+        getOptionLabel={(option) => option.symbol}
+        onChange={()=>{console.log('change')}}
+        onInputChange={debounce(handleInputChange, 500)}
+        filterOptions={x=>x}
+        renderOption={renderOption}
+        renderInput={renderInput}
+        className={classes.autocomplete}
       />
       <div className={classes.placeholder}>
         Counterparty Contents Placeholder
