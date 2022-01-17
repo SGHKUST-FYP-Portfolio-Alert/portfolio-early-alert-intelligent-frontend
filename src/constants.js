4623ac44253c3420c62810c17dc06a9000e529c6
Ng Ka Fong <richardng1126@gmail.com>
2022-01-17 22:52:08 +0800

    change serverURL to enable backup domain working

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-export const serverURL = "https://fyp-backend.cslix1.ml/";
+export const serverURL = `https://fyp-backend.${window.location.host}/`
+//export const serverURL = "https://fyp-backend.cslix1.ml/";
 //export const serverURL = "http://localhost:8080/"
