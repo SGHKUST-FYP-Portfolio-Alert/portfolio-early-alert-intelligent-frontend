d06cddf8f8b5809ce8fee7379c3845024b655a55
Ng Ka Fong <richardng1126@gmail.com>
2022-01-17 23:11:47 +0800

    bugfix to dynamic serverURL

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
-export const serverURL = `https://fyp-backend.${window.location.host}/`
+export const serverURL = `https://fyp-backend.${window.location.host.match(/fyp\.(.*)/)[1]}/`
 //export const serverURL = "https://fyp-backend.cslix1.ml/";
+//export const serverURL = "https://fyp-backend.richardng.nets.hk/"
 //export const serverURL = "http://localhost:8080/"
