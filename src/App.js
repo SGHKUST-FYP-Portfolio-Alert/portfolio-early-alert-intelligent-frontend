9c52bbb69381e2fddbd4c2b6f1a3c4f87c919113
Ng Ka Fong <richardng1126@gmail.com>
2022-01-24 21:16:11 +0800

    mobile layout fix: responsive drawer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import CounterpartyList from './pages/CounterpartyList';
 import Counterparty from './pages/Counterparty/Counterparty';
 import { ThemeProvider } from '@material-ui/styles';
 import NewCounterparty from './pages/NewCounterparty';
+import { useState } from 'react';
 
 import {
   BrowserRouter,
@@ -34,12 +35,10 @@ const theme = createTheme({
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: 'flex'
   },
   appBar: {
     zIndex: 1201,
-  },
-  sidebar: {
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -49,35 +48,47 @@ const useStyles = makeStyles((theme) => ({
   },
   page: {
     marginTop: 64,
-    marginLeft: 156,
+    flexGrow: 1,
+  }, 
+  menuButton: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    }
   }
 }));
 
 function App() {
 
   const classes = useStyles();
+  const [ mobileSidebarOpen, setMobileSidebarOpen ] = useState(false);
+
+  function handleClick(){
+    setMobileSidebarOpen(!mobileSidebarOpen)
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              Counterparty Risk Early Alert
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Sidebar className={classes.sidebar}/>
-        <div className={classes.page}>
-          <Switch>
-            <Route exact path="/" render={(props) => <Dashboard/>}/>
-            <Route exact path="/counterparty-list" render={(props) => <CounterpartyList/>}/>
-            <Route exact path="/counterparty" render={props => <Counterparty/>}/>
-            <Route exact path="/new-counterparty" render={props => <NewCounterparty/>}/>
-          </Switch>
+        <div className={classes.root}>
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleClick}>
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Counterparty Risk Early Alert
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Sidebar mobileSidebarOpen={mobileSidebarOpen} setMobileSidebarOpen={setMobileSidebarOpen}/>
+          <div className={classes.page}>
+            <Switch>
+              <Route exact path="/" render={(props) => <Dashboard/>}/>
+              <Route exact path="/counterparty-list" render={(props) => <CounterpartyList/>}/>
+              <Route exact path="/counterparty" render={props => <Counterparty/>}/>
+              <Route exact path="/new-counterparty" render={props => <NewCounterparty/>}/>
+            </Switch>
+          </div>
         </div>
       </BrowserRouter>
     </ThemeProvider>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,25 +3,62 @@ import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from "@material-ui/core";
+import Hidden from '@material-ui/core/Hidden';
 
+const useStyles = makeStyles((theme) => ({
+  sidebarList: {
+    marginTop: 64
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: 160,
+      flexShrink: 0,
+    },
+  }
+}));
 
 const Sidebar = (props) => {
 
-  const { history, className } = props;
+  const { history, mobileSidebarOpen, setMobileSidebarOpen } = props;
+
+  const classes = useStyles();
+
+  function handleClose(){
+    setMobileSidebarOpen(false)
+  }
+
+  const SidebarList = () => (
+    <List className={classes.sidebarList}>
+      <ListItem button key="Dashboard" onClick={()=> history.push('/')}>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+      <ListItem button key="Counterparty List" onClick={()=> history.push('/counterparty-list')}>
+        <ListItemText primary="Counterparty List" />
+      </ListItem>
+    </List>
+  )
 
   return (
-    <Drawer variant="permanent" anchor='left' className={className}>
-      <Toolbar />
-      <List>
-        <ListItem button key="Dashboard" onClick={()=> history.push('/')}>
-          <ListItemText primary="Dashboard" />
-        </ListItem>
-        <ListItem button key="Counterparty List" onClick={()=> history.push('/counterparty-list')}>
-          <ListItemText primary="Counterparty List" />
-        </ListItem>
-      </List>
-    </Drawer>
+    <nav className={classes.drawer}>
+    <Hidden xsDown implementation="css">
+      <Drawer variant="permanent" anchor='left'>
+        <SidebarList />
+      </Drawer>
+    </Hidden>
+    <Hidden smUp implementation="css">
+      <Drawer 
+        variant="temporary" 
+        anchor="left" 
+        open={mobileSidebarOpen} onClose={handleClose}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+      >
+        <SidebarList />
+      </Drawer>
+    </Hidden>
+    </nav>
   );
 
 };
